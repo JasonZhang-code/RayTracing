cc6635689ad32cbf032ac3688b44096d7e66fc4b
Jason Zhang <jasonzhang_zihan@hotmail.com>
2022-11-05 10:15:27 +1300

    Material Update

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -62,13 +62,13 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y) {
 	glm::vec3 color(0.0f);
 	float multiplier = 1.0f;
 
-	int bounces = 2;
+	int bounces = 5;
 	for (int i = 0; i < bounces; i++)
 	{
 		Renderer::HitPayLoad payload = TraceRay(ray);
 		if (payload.HitDistance < 0.0f)
 		{
-			glm::vec3 Skycolor = glm::vec3(0.0f, 0.0f, 0.0f);
+			glm::vec3 Skycolor = glm::vec3(0.6f, 0.7f, 0.9f);
 			color += Skycolor * multiplier;
 			break;
 		}
@@ -77,14 +77,15 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y) {
 		float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -lightDir), 0.0f); // == cos(angle)
 
 		const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
-		glm::vec3 sphereColor = sphere.Albedo;
+		const Material& material = m_ActiveScene->Materials[sphere.MaterialIndex];
+		glm::vec3 sphereColor = material.Albedo;
 		sphereColor *= lightIntensity;
 		color += sphereColor * multiplier;
 
-		multiplier *= 0.7;
+		multiplier *= 0.5;
 
 		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.00001f;
-		ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal);
+		ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal + material.Roughness * Walnut::Random::Vec3(-0.5, 0.5));
 	}
 
 	return glm::vec4(color, 1.0f);
